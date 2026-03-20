ef87d98faab91ed51cc165e5168ef9763367b347
Aditya Pandey <aditya.pandey2613@gmail.com>
2026-03-20 23:11:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py setup_google
+python manage.py setup_google --domain carpooling-c5sr.onrender.com
