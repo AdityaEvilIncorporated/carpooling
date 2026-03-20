840b36e51ec8fcd7098b2aad29c405745e915029
Aditya Pandey <aditya.pandey2613@gmail.com>
2026-03-20 23:25:33 +0530

    Update setup_google.py

--- accounts/management/commands/setup_google.py
+++ accounts/management/commands/setup_google.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.contrib.sites.models import Site
-
+import os
 
 class Command(BaseCommand):
     help = 'Sets the site domain for Google OAuth'
@@ -9,9 +9,22 @@ class Command(BaseCommand):
         parser.add_argument('--domain', default='127.0.0.1:8000')
 
     def handle(self, *args, **options):
+        from allauth.socialaccount.models import SocialApp
+
         domain = options['domain']
         site, created = Site.objects.get_or_create(id=1)
         site.domain = domain
         site.name = domain
         site.save()
+
+        client_id = os.environ.get('GOOGLE_CLIENT_ID', '')
+        secret = os.environ.get('GOOGLE_CLIENT_SECRET', '')
+
+        app, created = SocialApp.objects.get_or_create(provider='google')
+        app.name = 'Google'
+        app.client_id = client_id
+        app.secret = secret
+        app.save()
+        app.sites.add(site)
+
         self.stdout.write(self.style.SUCCESS(f'Site domain is now: {site.domain}'))
